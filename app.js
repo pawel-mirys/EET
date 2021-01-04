b3c7bd8b53b008d571f31abc73d68d70ec4032db
YoshiCity <pawel.mirys@gmail.com>
2021-01-04 04:28:30 +0100

    Bhp section

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const burger = document.querySelector(".burger");
 const links = document.querySelector(".links");
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
 const burgerBars = Array.from(burger.children);
 const cards = Array.from(document.querySelectorAll('.company-card'));
-
-
-
 
 burger.addEventListener('click', () => {
     links.classList.toggle('active');
@@ -13,8 +11,17 @@ burger.addEventListener('click', () => {
     })
 })
 
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        links.classList.contains('active') ? links.classList.remove('active') : undefined;
+        burgerBars.forEach((bar) => {
+            bar.classList.contains('active') ? bar.classList.remove('active') : undefined;
+        })
+    })
+})
+
 cards.forEach(card => {
     card.classList.contains('tameh') ? card.firstElementChild.src = './assets/images/tameh.png' : undefined;
     card.classList.contains('eko') ? card.firstElementChild.src = './assets/images/eko-energia.png' : undefined;
-    card.classList.contains('gzog') ? card.firstElementChild.src = './assets/images/gzog.png' : undefined;
-});
+    card.classList.contains('gzog') ? card.firstElementChild.src = './assets/images/GZOG.png' : undefined;
+})
